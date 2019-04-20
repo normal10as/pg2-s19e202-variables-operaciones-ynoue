3afcc0d086e572f53c78f0f924db0a7bb174f617
santiago ynoue <santi_777@hotmail.es>
2019-04-20 18:54:14 -0300

    hecho

---
+++ variable_tipos/variable_tipos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace variable_tipos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
