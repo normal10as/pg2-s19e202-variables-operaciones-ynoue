9f8c86e8269924ff80b588a626299e7b92fb8405
santiago ynoue <santi_777@hotmail.es>
2019-04-20 20:18:45 -0300

    Hecho1.3

---
+++ concatenacion/concatenacion/Program.vb
@@ -0,0 +1,23 @@
+Imports System
+
+Module Program
+    Sub Program()
+
+        Dim nombreempresa As String
+        Dim nombrecalle As String
+        Dim alturacalle As String
+        Dim inicioactividad As Date
+
+        nombreempresa = "Nvidia"
+        nombrecalle = "faker"
+        alturacalle = 126
+        inicioactividad = #10/5/2014#
+
+
+
+
+
+
+
+    End Sub
+End Module
