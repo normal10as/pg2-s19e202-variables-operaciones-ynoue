95bf5cde3167aaf0d73f61ab57f503e4799c0e01
santiago ynoue <santi_777@hotmail.es>
2019-04-20 19:57:39 -0300

    hecho

--- variables/variables_consola/Program.vb
+++ variables/variables_consola/Program.vb
@@ -1,7 +1,18 @@
 Imports System
 
 Module Program
-    Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+    Sub Main()
+        Dim nombre As String
+        Dim apellido As String
+        Dim fechanacimiento As Date
+
+        Console.WriteLine("ingrese el nombre: ")
+        nombre = Console.ReadLine()
+        Console.WriteLine("ingrese el apellido: ")
+        apellido = Console.ReadLine()
+        Console.WriteLine("ingrese la fecha de nacimiento: ")
+        fechanacimiento = Console.ReadLine()
+
+        Console.WriteLine(apellido & " " & nombre & " nacio el " & fechanacimiento)
     End Sub
 End Module
