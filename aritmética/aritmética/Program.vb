75c403993a408b484b274ba68933d1911a02cfa5
santiago ynoue <santi_777@hotmail.es>
2019-04-22 15:53:07 -0300

    Hecho

---
+++ aritmética/aritmética/Program.vb
@@ -0,0 +1,55 @@
+Imports System
+
+Module Program
+    Sub Main()
+
+
+        Dim enterouno As Integer
+
+        Dim enterodos As Integer
+
+        Dim flotanteuno As Double
+        Dim flotantedos As Double
+
+
+        enterouno = 10
+
+        enterodos = 20
+
+        flotanteuno = 30.5
+
+        flotantedos = 40.6
+
+        Console.WriteLine("enterouno mas enterodos ")
+        Console.WriteLine((enterouno + enterodos))
+        Console.WriteLine("enterouno mas flotanteuno")
+        Console.WriteLine((enterouno + flotanteuno))
+        Console.WriteLine("flotatanteuno mas flotantedos")
+        Console.WriteLine((flotanteuno + flotantedos))
+        Console.WriteLine("enterouno menos enterodos")
+        Console.WriteLine((enterouno - enterodos))
+        Console.WriteLine("enterouno mas flotanteuno")
+        Console.WriteLine((enterouno - flotanteuno))
+        Console.WriteLine("flotanteuno  menos flotantedos")
+        Console.WriteLine((flotanteuno * flotantedos))
+        Console.WriteLine("enterouno por enterodos ")
+        Console.WriteLine((enterouno * enterodos))
+        Console.WriteLine("enterouno por flotanteuno")
+        Console.WriteLine((enterouno * flotanteuno))
+        Console.WriteLine("flotatanteuno por flotantedos")
+        Console.WriteLine((flotanteuno * flotantedos))
+        Console.WriteLine("enterouno dividido enterodos ")
+        Console.WriteLine((enterouno / enterodos))
+        Console.WriteLine("enterouno dividido flotanteuno")
+        Console.WriteLine((enterouno / flotanteuno))
+        Console.WriteLine("flotatanteuno dividido flotantedos")
+        Console.WriteLine((flotanteuno / flotantedos))
+
+
+
+
+
+
+
+    End Sub
+End Module
