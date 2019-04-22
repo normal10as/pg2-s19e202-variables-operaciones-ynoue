f7b6cee5b7a1ed5ca3baaf96dccdb84895af4205
santiago ynoue <santi_777@hotmail.es>
2019-04-22 16:30:23 -0300

    Hecho

---
+++ 1.5ecuacion/1.5ecuacion/Program.vb
@@ -0,0 +1,21 @@
+Imports System
+
+Module Program
+    Sub Main()
+        Dim a As Integer
+        Dim b As Integer
+        Dim c As Integer
+        Dim x As Integer
+
+
+        a = 1
+        b = 5
+        c = 2
+
+        x = ((b ^ 2) - (4 * a * c)) / (2 * a)
+        Console.WriteLine(x)
+
+
+
+    End Sub
+End Module
