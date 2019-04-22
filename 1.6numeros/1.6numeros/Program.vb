998c2df006b8df46ba1de102f1e0c612736c0b94
santiago ynoue <santi_777@hotmail.es>
2019-04-22 16:52:21 -0300

    Hecho

---
+++ 1.6numeros/1.6numeros/Program.vb
@@ -0,0 +1,31 @@
+Imports System
+
+Module Program
+    Sub Main()
+
+        Dim uno, dos, tres, cuatro, sumatoria, total As Integer
+
+        Console.WriteLine("ingrese un numero : ")
+        uno = Console.ReadLine()
+        Console.WriteLine("ingrese un numero : ")
+        dos = Console.ReadLine()
+        Console.WriteLine("ingrese un numero : ")
+        tres = Console.ReadLine()
+        Console.WriteLine("ingrese un numero : ")
+        cuatro = Console.ReadLine()
+
+
+        sumatoria = sumatoria + (uno + dos + tres + cuatro)
+        Console.WriteLine(sumatoria)
+        total = total + (sumatoria / 2)
+        Console.WriteLine(total)
+
+
+
+
+
+
+
+
+    End Sub
+End Module
