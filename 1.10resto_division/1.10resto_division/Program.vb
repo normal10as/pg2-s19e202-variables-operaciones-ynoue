d163e965af6aa16677002a1b8f790b07675f3d94
santiago ynoue <santi_777@hotmail.es>
2019-05-02 16:02:40 -0300

    Hecho

---
+++ 1.10resto_division/1.10resto_division/Program.vb
@@ -0,0 +1,18 @@
+Imports System
+
+Module Program
+    Sub Main()
+        Dim nume As Integer
+        Dim resultado As Integer
+        Console.WriteLine("ingrese un numero:")
+        nume = Console.ReadLine()
+        resultado = nume / 2
+
+        Console.WriteLine("su numero dividido por dos: {0}", Math.Abs(resultado))
+
+
+
+
+
+    End Sub
+End Module
