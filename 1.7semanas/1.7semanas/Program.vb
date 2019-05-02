c526e6c6c6398c267a4bd34d769480f4ce0f2d1f
santiago ynoue <santi_777@hotmail.es>
2019-05-02 15:35:29 -0300

    Hecho

--- 1.7semanas/1.7semanas/Program.vb
+++ 1.7semanas/1.7semanas/Program.vb
@@ -3,18 +3,20 @@ Imports System
 Module semanas
     Sub Main()
 
-        Const diasanio As Integer = 365
-        Const dialaboral As Integer = 7
-        Const diasemana As Integer = 5
+        Const diasemana As Integer = 7
+        Const horadia As Integer = 24
+        Const minutodia As Integer = 60
 
-        Dim semanasanio As Integer
-        Dim semanalaboral As Integer
+        Dim numerocualquiera As Byte
 
-        semanasanio = (diasanio / dialaboral)
-        semanalaboral = ((diasanio / diasemana) - (diasanio / dialaboral))
+        Console.WriteLine("ingrese un numero de la semana:")
+        numerocualquiera = Console.ReadLine()
 
-        Console.WriteLine("las semanas del anio son : {0}", semanasanio)
-        Console.Write("las semanas laborales  son : {0}", semanalaboral)
+
+        Console.WriteLine(numerocualquiera / diasemana)
+        Console.WriteLine(numerocualquiera * horadia)
+        Console.WriteLine(numerocualquiera * minutodia)
+
 
 
 
