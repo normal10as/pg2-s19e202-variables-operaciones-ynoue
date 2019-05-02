fca347bea012bb2a04ca0bf092f0652580beb260
santiago ynoue <santi_777@hotmail.es>
2019-05-02 15:52:08 -0300

    Hecho

---
+++ 1.9interes/1.9interes/Program.vb
@@ -0,0 +1,27 @@
+Imports System
+
+Module Program
+    Sub main()
+        Dim m As Single
+        Dim r As Single
+        Dim t As UShort
+        Dim i As Single
+
+        Console.WriteLine("ingrese el monto:")
+        m = Console.ReadLine()
+        Console.WriteLine("ingrse el interes:")
+        r = Console.ReadLine
+        Console.WriteLine("ingrese el tiempo:")
+        t = Console.ReadLine()
+
+        i = m * r * t / 360 * 100
+        Console.WriteLine("el interes es {0} ", i)
+
+
+
+
+
+
+
+    End Sub
+End Module
